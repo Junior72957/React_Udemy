b28a02add0e21c3ef1fb314f927cf7834e79c7fb
Eduardo Junior <62727558+Junior72957@users.noreply.github.com>
2022-06-29 20:02:47 -0300

    fundamentos

---
+++ hello-world/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1 className= "cor">Hello World React</h1>
+      <h2>agora sim meu parceiro</h2>
+      <h1>garai</h1>
+
+    </div>
+  );
+}
+
+export default App;
+
+
