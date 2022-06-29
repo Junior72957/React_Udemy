860656dd3d3135aa72fcb98dd9da4604e8291ed3
Eduardo Junior <62727558+Junior72957@users.noreply.github.com>
2022-06-29 20:18:22 -0300

    aula 2
    fundamentos

---
+++ 2_fundamentos/aula_2_fundamentos/src/App.js
@@ -0,0 +1,16 @@
+//components
+import FirstComponent from './components/FirstComponent';
+//css /syles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+          <h1 className= "titulo">Aula Fundamentos</h1>
+       <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
