7e870b47b29082b4c057ea37d847dba701477767
Eduardo Junior <62727558+Junior72957@users.noreply.github.com>
2022-07-19 19:31:45 -0300

    Finalizando capitulo

--- 7_req_HTTP/httpreact/src/App.js
+++ 7_req_HTTP/httpreact/src/App.js
@@ -2,76 +2,107 @@ import "./App.css";
 
 import { useState, useEffect } from "react";
 
+// 4 - custom hook
+import { useFetch } from "./hooks/useFetch";
+
+// 8 - errar url para mostrar erro
+// "http://localhost:3001/products"
 const url = "http://localhost:3000/products";
 
 function App() {
-const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-const [name, setName] = useState("");
-const [price, setPrice] = useState("");
+  // 4 - custom hook e 5 - refactor post
+  const { data: items, httpConfig, loading, error } = useFetch(url);
 
-  //1 - resgatando dados
-  useEffect(() => {
-    async function fetchData() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
-    const res = await fetch (url)
+  // 1 - resgatando dados
+  // useEffect(async () => {
+  //   const res = await fetch("http://localhost:3000/products");
 
-    const data = await res.json()
+  //   const data = await res.json();
 
-    setProducts(data)
-  }
-  fetchData();
-  }, []);
+  //   setProducts(data);
+  // }, []);
 
-    // 2- add  de produtos
+  // 2 - add product
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
+    const product = {
+      name,
+      price,
+    };
 
-      const product = {
-        name, 
-        price,
-      };
+    // const res = await fetch("http://localhost:3000/products", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify(product),
+    // });
 
-      const res = await fetch(url, {
-        method: "POST",
-        headers: {
-          "content-Type": "application/json"
-        },
-        body: JSON.stringify(product),
-      });
+    // const addedProduct = await res.json();
 
-    }
+    // 3 - carregamento dinâmico
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    // 5 - refatorar post
+    httpConfig(product, "POST");
+
+    setName("");
+    setPrice("");
+  };
+
+  /* 9 - desafio */
+  const handleRemove = (id) => {
+    httpConfig(id, "DELETE");
+  };
 
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      {/* 6 - state de loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
       <ul>
-        {products.map((product) => (
-          <li key={product.id}> 
-          {product.name} - R$: {product.price}</li>
-        ))}
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+              {/* 9 - desafio */}
+              <button onClick={() => handleRemove(product.id)}>Excluir</button>
+            </li>
+          ))}
       </ul>
+
       <div className="add-product">
+        <p>Adicionar produto:</p>
         <form onSubmit={handleSubmit}>
-          <label >
+          <label>
             Nome:
-            <input type="text" 
-            value={name} 
-            name ="name" 
-            onChange={(e) => setName(e.target.value)}/>
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
           </label>
-          <label >
+          <label>
             Preço:
-            <input type="number" 
-            value={price} 
-            name ="price" 
-            onChange={(e) => setPrice(e.target.value)}/>
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
           </label>
-          <input type="submit" value="Criar"/>
+          {/* 7 - state de loading no post */}
+          {loading ? <p>Aguarde!</p> : <input type="submit" value="Criar" />}
         </form>
       </div>
-      
     </div>
   );
 }
